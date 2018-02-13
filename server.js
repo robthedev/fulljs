6ccf67f6b7c06144feaf352735d128e2b50f0fef
rob dev <robthewebdev@gmail.com>
2018-02-13 15:28:19 -0600

    added ejs for templating

--- server.js
+++ server.js
@@ -1,20 +1,26 @@
 import config from './config';
 import express from 'express';
 import fs from 'fs';
+import apiRouter from './api';
 
 const server = express();
 
+server.set('view engine', 'ejs');
+
 server.get('/', (req, res) => {
-    res.send('hello ');
+    res.render('index', {
+        content: 'mitel'
+    });
 });
 
+server.use('/api', apiRouter);
 server.use(express.static('public'));
 
-server.get('/about.html', (req, res) => {
+/* server.get('/about.html', (req, res) => {
     fs.readFile('./about.html', (err, data) => {
         res.send(data.toString());
     });
-});
+}); */
 
 server.listen(config.port, () => {
     console.log('express listening on port ', config.port);
