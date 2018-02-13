f26026e9bc91a91aa8b915350af238761ea99569
rob dev <robthewebdev@gmail.com>
2018-02-13 14:27:07 -0600

    testing of express server on localhost

--- server.js
+++ server.js
@@ -1,3 +1,21 @@
 import config from './config';
+import express from 'express';
+import fs from 'fs';
 
-console.log(config);
+const server = express();
+
+server.get('/', (req, res) => {
+    res.send('hello ');
+});
+
+server.use(express.static('public'));
+
+server.get('/about.html', (req, res) => {
+    fs.readFile('./about.html', (err, data) => {
+        res.send(data.toString());
+    });
+});
+
+server.listen(config.port, () => {
+    console.log('express listening on port ', config.port);
+});
